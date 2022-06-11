190f12d316df02169f8eff13dcb413f2722bcf5d
Diogenesoftoronto <87236699+Diogenesoftoronto@users.noreply.github.com>
2022-06-11 19:49:38 -0400

    random function for workshop

---
+++ src/script.js
@@ -0,0 +1,15 @@
+  // first we must create a function to do hold the code for our solution
+function randomStudent(arrayOfStudents, chance) {
+  const amountOfStudents = arrayOfStudents.length
+  const ratio = Math.floor(chance/100)
+  const numberOfNulls = amountOfStudents/ratio
+  // the ratio of students to nulls (in the array) multiplied by one hundred should be the same as chance
+  let lotteryArray = [...arrayOfStudents]
+  // we have to create a loop that adds that ratio to the array
+  while (lotteryArray.length - amountOfStudents) < numberOfNulls) {lotteryArray.push(null)};
+  
+  // we must create a random integer between 0 and (the amount of students in the class) - 1 
+  const randomNumber = Math.floor(Math.random() * lotteryArray.length);
+  const winner = lotteryArray[randomNumber]
+  return winner
+}
