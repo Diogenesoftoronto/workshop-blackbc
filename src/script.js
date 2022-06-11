cce6ee80bdf6484c1c0d21c87310d92cf2bad1de
Diogenesoftoronto <87236699+Diogenesoftoronto@users.noreply.github.com>
2022-06-11 19:58:09 -0400

    finish function and fix typo

--- src/script.js
+++ src/script.js
@@ -1,15 +1,18 @@
   // first we must create a function to do hold the code for our solution
-function randomStudent(arrayOfStudents, chance) {
+function lotteryWinner(arrayOfStudents, chance) {
   const amountOfStudents = arrayOfStudents.length
   const ratio = Math.floor(chance/100)
-  const numberOfNulls = amountOfStudents/ratio
+  const numberOfNulls = amountOfStudents/ratio;
   // the ratio of students to nulls (in the array) multiplied by one hundred should be the same as chance
-  let lotteryArray = [...arrayOfStudents]
+  // copy array of students to the lottery array
+  let lotteryArray = [...arrayOfStudents];
   // we have to create a loop that adds that ratio to the array
-  while (lotteryArray.length - amountOfStudents) < numberOfNulls) {lotteryArray.push(null)};
-  
+  while ((lotteryArray.length - amountOfStudents) < numberOfNulls) {lotteryArray.push(null)}
+
   // we must create a random integer between 0 and (the amount of students in the class) - 1 
   const randomNumber = Math.floor(Math.random() * lotteryArray.length);
   const winner = lotteryArray[randomNumber]
+  
   return winner
 }
+
