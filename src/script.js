a0df77714ab2e232cee4ba0402221317bfe6f2c3
Diogenesoftoronto <87236699+Diogenesoftoronto@users.noreply.github.com>
2022-06-12 16:03:44 -0400

    spacing

--- src/script.js
+++ src/script.js
@@ -6,13 +6,13 @@ function lotteryWinner(arrayOfStudents, chance) {
   // the ratio of students to nulls (in the array) multiplied by one hundred should be the same as chance
   // copy array of students to the lottery array
   let lotteryArray = [...arrayOfStudents];
-  // we have to create a loop that adds that ratio to the array
+  // we have to create a loop that adds the nulls to the array
   while ((lotteryArray.length - amountOfStudents) < numberOfNulls) {lotteryArray.push(null)}
-
   // we must create a random integer between 0 and (the amount of students in the class) - 1 
   const randomNumber = Math.floor(Math.random() * lotteryArray.length);
+  // the winner is going to be whichever value is at the index of the random number.
   const winner = lotteryArray[randomNumber]
-  
+  // return the winner and we are ready to go
   return winner
 }
 
