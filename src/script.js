40572b103a77a96ab06977d165137493a264d730
Diogenesoftoronto <87236699+Diogenesoftoronto@users.noreply.github.com>
2022-06-12 16:55:30 -0400

    format

--- src/script.js
+++ src/script.js
@@ -1,18 +1,31 @@
-  // first we must create a function to do hold the code for our solution
+// first we must create a function to do hold the code for our solution
 function lotteryWinner(arrayOfStudents, chance) {
-  const amountOfStudents = arrayOfStudents.length
-  const ratio = Math.floor(chance/100)
-  const numberOfNulls = amountOfStudents/ratio;
+  const amountOfStudents = arrayOfStudents.length;
+  const ratio = Math.floor(chance / 100);
+  const numberOfNulls = amountOfStudents / ratio;
   // the ratio of students to nulls (in the array) multiplied by one hundred should be the same as chance
   // copy array of students to the lottery array
   let lotteryArray = [...arrayOfStudents];
   // we have to create a loop that adds the nulls to the array
-  while ((lotteryArray.length - amountOfStudents) < numberOfNulls) {lotteryArray.push(null)}
-  // we must create a random integer between 0 and (the amount of students in the class) - 1 
+  while (lotteryArray.length - amountOfStudents < numberOfNulls) {
+    lotteryArray.push(null);
+  }
+  // we must create a random integer between 0 and (the amount of students in the class) - 1
   const randomNumber = Math.floor(Math.random() * lotteryArray.length);
   // the winner is going to be whichever value is at the index of the random number.
-  const winner = lotteryArray[randomNumber]
+  const winner = lotteryArray[randomNumber];
   // return the winner and we are ready to go
-  return winner
+  return winner;
 }
 
+// get the students from the submit button and put them into an array. Display that array to the document
+function getStudent(e) {
+  // arrayOfstudents.push(e.data)
+  e.preventDefault();
+  console.table(e);
+}
+const studentForm = document.querySelector(".form-lottery");
+
+studentForm?.addEventListener("submit", getStudent);
+
+console.log("helloworld", studentForm);
