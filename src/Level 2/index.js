fedd2f06c79a49799598a2baa9187037b588a226
Diogenesoftoronto <87236699+Diogenesoftoronto@users.noreply.github.com>
2022-06-12 22:56:43 -0400

    add the second challenge

---
+++ src/Level 2/index.js
@@ -0,0 +1,7 @@
+// create an array of students 
+
+
+// write a function that takes an array of students and returns a random student but with only a 50% chance of any student winning
+
+// call the function and display the result as an alert on the site
+alert
