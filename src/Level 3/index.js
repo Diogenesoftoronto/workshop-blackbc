b8c89cdd0e15083bb58da1bfb05053fc0d8cf89b
Diogenesoftoronto <87236699+Diogenesoftoronto@users.noreply.github.com>
2022-06-12 22:56:53 -0400

    add the third challenge

---
+++ src/Level 3/index.js
@@ -0,0 +1,6 @@
+// create an array of students 
+
+// write a function that takes an array of students and returns a random student but with a variable chance of any student winning
+
+// call the function and display the result as an alert on the site with a delay 5 secs
+setTimeout
