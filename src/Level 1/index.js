00b8a36e858404ccc77c44a3b3cb9b1f5692d004
Diogenesoftoronto <87236699+Diogenesoftoronto@users.noreply.github.com>
2022-06-12 22:37:56 -0400

    Create index.js

---
+++ src/Level 1/index.js
@@ -0,0 +1,14 @@
+// create an list of students
+
+
+// write a function that takes an array of students and returns a random student
+function getRandomStudent(students) {
+  randomNumber = Math.floor(Math.random() * students.length);
+  // This function is incomplete your challenge is to get it to work! 
+  return null
+}
+
+// call the function and log the result to the screen
+
+
+// give the user an alert message with the winner's name
